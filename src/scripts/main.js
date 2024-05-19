2d0d8e456701bcfb9f019494024a8b3f166c963c
Lucas Fernandes <lucasuzumaki69@hotmail.com>
2024-05-19 18:16:56 -0300

    ajustes mobile

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,21 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const faq = document.querySelectorAll('[data-faq-question]')
+    const herosection = document.querySelector('.hero');
+    const alturahero = herosection.clientHeight;
+
+
+    window.addEventListener('scroll', function(){
+        const valorAtual = window.scrollY
+
+        if(valorAtual < alturahero){
+            ocultaheader();
+        }else{
+            exibiheader();
+        }
+    })
+
+
 
     for(let i = 0; i < buttons.length; i++){
         buttons[i].addEventListener('click', function(botoes){
@@ -9,10 +25,29 @@ document.addEventListener('DOMContentLoaded', function(){
             removeBotoes();
             aba.classList.add('shows__list--is-active')
             botoes.target.classList.add('shows__tabs__button--is-active')
-
         })
+
+        for( let i = 0; i < faq.length; i++){
+            faq[i].addEventListener('click', abreoufecharesposta);
+        }
     }
 })
+
+function ocultaheader(){
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden')
+}
+function exibiheader(){
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden')
+}
+
+function abreoufecharesposta(elemento){
+    const classe =  'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+    
+        elementoPai.classList.toggle(classe)
+}
 
 function removeBotoes(){
     const buttons = document.querySelectorAll('[data-tab-button]');
